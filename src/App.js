1284ad3d87d01f778c52444ec77f269d1a8e8d31
Rahul21sai <110412514+Rahul21sai@users.noreply.github.com>
2023-10-22 23:36:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import TextBox from "./TextBox.js";
+
+function App() {
+  return (
+    <div className="">
+      <div className="border rounded container text-center" id="main">
+        <h1> Responsive Paragraph Word Counter</h1>
+        <TextBox />
+        <br/>
+        <br />
+        
+      </div>
+    </div>
+  );
+}
+export default App;
